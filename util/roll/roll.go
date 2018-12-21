d15114122b823d47c8897c10683e45a0615222a3
Octopus Octopus <oct2pus@jade.moe>
2018-12-21 15:18:39 -0600

    remove f8 related functions from vriska.go add more comments to roll.go

--- util/roll/roll.go
+++ util/roll/roll.go
@@ -1,11 +1,13 @@
-package roll
+package dice
 
 import (
-	"github.com/oct2pus/vriskabot/util/logging"
 	"math/rand"
 	"regexp"
 	"strconv"
 	"time"
+
+	"github.com/oct2pus/botutil/etc"
+	"github.com/oct2pus/vriskabot/util/logging"
 )
 
 type roll struct {
@@ -14,13 +16,14 @@ type roll struct {
 	Mod    int64 // Modifier to roll
 }
 
+// New generates a new roll struct.
 func New(a int64, b int64, c int64) roll {
 	return roll{a, b, c}
 }
 
-// returns a series of random numbers (determined by die.sizeOfDie) in an int64
-// slice, which is as large as die.numberOfDie
-func RollTable(rolls roll) []int64 {
+// Table returns a series of random numbers (determined by die.sizeOfDie) in an int64
+// slice the size of rolls.Amount.
+func Table(rolls roll) []int64 {
 	var table []int64
 	seed := time.Now()
 
@@ -34,10 +37,10 @@ func RollTable(rolls roll) []int64 {
 
 }
 
-// breaks the roll into a string slice
+// Slice breaks the roll into a string slice
 // code assumes you've checked input prior
-// TODO: add a break/error state
-func DiceSlice(input string) []string {
+// TODO: add a break/error state.
+func Slice(input string) []string {
 	// [0] is the number of dice being rolled
 	// [1] is the type of die
 	// [2] is the modifier direction (positive/negative)
@@ -45,34 +48,106 @@ func DiceSlice(input string) []string {
 
 	divider := regexp.MustCompile("[0-9]+|[\\+|-]")
 
-	dieSlice := divider.FindAllString(input, -1)
+	slice := divider.FindAllString(input, -1)
 
-	if len(dieSlice) <= 2 {
-		dieSlice = append(dieSlice, "+")
+	if len(slice) <= 2 {
+		slice = append(slice, "+")
 	}
-	if len(dieSlice) <= 3 {
-		dieSlice = append(dieSlice, "0")
+	if len(slice) <= 3 {
+		slice = append(slice, "0")
 	}
-	return dieSlice
+	return slice
 }
 
-// turns the dieSlice string slice into a dieRoll object
-func FromStrings(diceSlice []string) roll {
+// FromStringSlice takes slice (a []string) and outputs a roll struct.
+func FromStringSlice(slice []string) roll {
 	var die roll
 	var err error
 
-	die.Amount, err = strconv.ParseInt(diceSlice[0], 0, 0)
+	die.Amount, err = strconv.ParseInt(slice[0], 0, 0)
 	logging.CheckError(err)
-	die.Size, err = strconv.ParseInt(diceSlice[1], 0, 0)
+	die.Size, err = strconv.ParseInt(slice[1], 0, 0)
 	logging.CheckError(err)
-	die.Mod, err = strconv.ParseInt(diceSlice[3], 0, 0)
+	die.Mod, err = strconv.ParseInt(slice[3], 0, 0)
 	logging.CheckError(err)
 
 	// if number is negative is negative
-	if diceSlice[2] == "-" {
+	if slice[2] == "-" {
 		die.Mod = 0 - die.Mod
 	}
 
 	return die
 
+}
+
+// Takes  and returns a 'table' formatted for an embed
+func FormatTable(table []string) string {
+	fieldValue := "`"
+	for x := 0; x < len(table); x++ {
+		if x%4 == 0 && x != 0 {
+			fieldValue += "`\n`"
+		}
+		if x != 0 && x%4 != 0 {
+			fieldValue += "\t"
+		}
+		fieldValue += "|" +
+			etc.ToCenter(table[x]) + "|"
+	}
+
+	fieldValue += "`"
+
+	return fieldValue
+}
+
+// Determines what image to use
+func dieImage(face int64) string {
+	switch {
+	case face <= 4:
+		return "https://raw.githubusercontent.com/oct2pus/vriskabot/master/emoji/d4.png"
+	case face <= 6:
+		return "https://raw.githubusercontent.com/oct2pus/vriskabot/master/emoji/d6.png"
+	case face <= 8:
+		return "https://raw.githubusercontent.com/oct2pus/vriskabot/master/emoji/d8.png"
+	case face <= 10:
+		return "https://raw.githubusercontent.com/oct2pus/vriskabot/master/emoji/d10.png"
+	case face <= 12:
+		return "https://raw.githubusercontent.com/oct2pus/vriskabot/master/emoji/d12.png"
+	default:
+		return "https://raw.githubusercontent.com/oct2pus/vriskabot/master/emoji/d20.png"
+	}
+}
+
+// getTotal gets the total value from an int64 slice
+func getTotal(arr []int64) int64 {
+
+	sum := int64(0)
+	for x := 0; x < len(arr); x++ {
+		sum += arr[x]
+	}
+
+	return sum
+}
+
+// getHighest gets the highest value from an int64 slice
+func getHighest(arr []int64) int64 {
+	highest := int64(0)
+	for x := 0; x < len(arr); x++ {
+		if highest < arr[x] {
+			highest = arr[x]
+		}
+	}
+
+	return highest
+}
+
+// getLowest gets the lowest value from an int64 slice
+func getLowest(arr []int64) int64 {
+	lowest := arr[0]
+	for x := 1; x < len(arr); x++ {
+		if lowest > arr[x] {
+			lowest = arr[x]
+		}
+	}
+
+	return lowest
 }
